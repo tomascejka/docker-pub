fd5cbabb04c3e19aae9c91957c5fa6a5a5a2f9b3
Tomáš Čejka <44152+tomascejka@users.noreply.github.com>
2022-10-09 17:38:40 +0200

    Delete terminal.bat

--- 00-minimalistic-java-image/terminal.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-rem
-rem @see https://learn.microsoft.com/cs-cz/windows/terminal/
-rem
-
-start wt -d .
-rem start wt -p "Windows PowerShell"
-rem start wt -p "Command Prompt" ; split-pane -p "Windows PowerShell" ; split-pane -p "Windows PowerShell v2"
